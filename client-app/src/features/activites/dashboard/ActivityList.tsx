5345bdae62c99711dbfd516ec4d47c93b3296e99
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2021-02-21 15:18:47 +0000

    Use Mobx to manage state in client-app
    Section 7 67-75
    Created a store and moved all state and functions to change state into
    this store.

--- client-app/src/features/activites/dashboard/ActivityList.tsx
+++ client-app/src/features/activites/dashboard/ActivityList.tsx
@@ -1,17 +1,14 @@
+import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent } from 'react'
 import { useState } from 'react';
 import { Button, Item, Label, Segment } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import { useStore } from '../../../app/stores/store';
 
-interface Props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
 
-}
 
-export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+export default observer(function ActivityList() {
+    const { activityStore } = useStore();
+    const {deleteActivity, activities, loading} = activityStore
     const [target, setTarget] = useState('');
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
@@ -19,6 +16,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
         deleteActivity(id);
     }
 
+    
     return (
         <Segment>
             <Item.Group divided>
@@ -33,11 +31,11 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                             </Item.Description>
                             <Item.Extra>
                                 <Button
-                                    onClick={() => selectActivity(activity.id)}
+                                    onClick={() => activityStore.selectActivity(activity.id)}
                                     floated='right' content='View' color='blue' />
                                 <Button
                                     name={activity.id}
-                                    loading={submitting && target === activity.id}
+                                    loading={loading && target === activity.id}
                                     onClick={(e) => handleActivityDelete(e, activity.id)}
                                     floated='right' content='Delete' color='red' />
                                 <Label basic content={activity.category} />
@@ -48,4 +46,4 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
             </Item.Group>
         </Segment>
     )
-}
+})
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css'
+import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
   <React.StrictMode>
+    <StoreContext.Provider value={store}>
     <App />
+    </StoreContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
