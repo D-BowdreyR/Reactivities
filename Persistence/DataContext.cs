dbbbdc95e13795be51fca8bd3551828a561aaa15
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2021-04-02 20:36:44 +0100

    End of section 14
    Related Entities and Authorization behaviour for users

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -14,6 +14,8 @@ namespace Persistence
 
         public DbSet<Activity> Activities { get; set; }
 
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -23,9 +25,21 @@ namespace Persistence
                     new Value {Id = 1, Name = "Value 101"},
                     new Value {Id = 2, Name = "Value 102"},
                     new Value {Id = 3, Name = "Value 103"}
-                    
                 );
-            
+
+            builder.Entity<ActivityAttendee>(x => 
+            x.HasKey(aa => 
+            new { aa.AppUserId, aa.ActivityId }));
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
         }
     }
 }
