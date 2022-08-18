2edc4c919010d7bcfc8107586a7fb104e5ea033a
Daniel Bowdrey-Roberts <Daniel.Bowdrey-Roberts@uhs.nhs.uk>
2022-08-18 09:22:17 +0100

    add signalr package to client add comment model and comment mobx store to client

---
+++ client-app/src/app/stores/commentStore.ts
@@ -0,0 +1,45 @@
+import { HubConnection, HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import { makeAutoObservable, runInAction } from "mobx";
+import { ChatComment } from "../models/comment";
+import { store } from "./store";
+
+export default class CommentStore {
+    comments: ChatComment[] = [];
+    hubConnection: HubConnection | null = null;
+
+    constructor() {
+        makeAutoObservable(this);
+        
+    }
+
+    createHubConnection = (activityId: string) => {
+        if (store.activityStore.selectedActivity) {
+            this.hubConnection = new HubConnectionBuilder()
+                .withUrl('http://localhost:5000/chat?activityId=' + activityId, {
+                    accessTokenFactory: () => store.userStore.user?.token!
+                })
+                .withAutomaticReconnect()
+                .configureLogging(LogLevel.Information)
+                .build();
+            
+            this.hubConnection.start().catch(error => console.log('Error establishing the connection: ', error))
+            // string arg must match the callback from signalR within the ChatHub
+            this.hubConnection.on('LoadComments', (comments: ChatComment[]) => {
+                runInAction(() => this.comments = comments);
+            
+                this.hubConnection?.on('ReceiveComment', (comments: ChatComment) => {
+                    runInAction(() => this.comments.push(comments));
+                })
+            })
+        }
+    }
+
+    stopHubConnection = () => {
+        this.hubConnection?.stop().catch(error => console.log('Error stopping connection: ', error))
+    }
+
+    clearComments = () => {
+        this.comments = [];
+        this.stopHubConnection();
+    }
+}
