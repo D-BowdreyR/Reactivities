85dd3594972bad9f91818521316041b6b13b8590
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2022-08-31 18:19:15 +0100

    add add comment single r client feature

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,66 +1,80 @@
-import { observer } from 'mobx-react-lite'
-import React, { useEffect } from 'react'
+import { Formik, Form } from 'formik';
+import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import {Segment, Header, Comment, Form, Button} from 'semantic-ui-react'
+import { Segment, Header, Comment, Button } from 'semantic-ui-react';
+import MyTextArea from '../../../app/common/form/MyTextArea';
 import { useStore } from '../../../app/stores/store';
 
 interface Props {
-    activityId: string;
+  activityId: string;
 }
 
-export default observer(function ActivityDetailedChat({activityId}: Props) {
-    const { commentStore } = useStore();
+export default observer(function ActivityDetailedChat({ activityId }: Props) {
+  const { commentStore } = useStore();
 
-    useEffect(() => {
-        
-        if (activityId) {
-            commentStore.createHubConnection(activityId);
+  useEffect(() => {
+    if (activityId) {
+      commentStore.createHubConnection(activityId);
+    }
 
-        }
-    
-      return () => {
-          commentStore.clearComments();
-      }
-    }, [commentStore, activityId])
-    
-    
-    return (
-        <>
-            <Segment
-                textAlign='center'
-                attached='top'
-                inverted
-                color='teal'
-                style={{border: 'none'}}
-            >
-                <Header>Chat about this event</Header>
-            </Segment>
-            <Segment attached>
-                <Comment.Group>
-                    {commentStore.comments.map(comment => (
-                        <Comment key={comment.id}>
-                        <Comment.Avatar src={comment.image || '/assets/user.png'}/>
-                        <Comment.Content>
-                                <Comment.Author as={Link} to={`/profiles/${comment.username}}`}>{comment.displayName}</Comment.Author>
-                            <Comment.Metadata>
-                                    <div>{comment.createdAt}</div>
-                            </Comment.Metadata>
-                            <Comment.Text>{comment.body}</Comment.Text>
-                        </Comment.Content>
-                    </Comment>
-                    ))}
-                    <Form reply>
-                        <Form.TextArea/>
-                        <Button
-                            content='Add Reply'
-                            labelPosition='left'
-                            icon='edit'
-                            primary
-                        />
-                    </Form>
-                </Comment.Group>
-            </Segment>
-        </>
+    return () => {
+      commentStore.clearComments();
+    };
+  }, [commentStore, activityId]);
 
-    )
-})
+  return (
+    <>
+      <Segment
+        textAlign='center'
+        attached='top'
+        inverted
+        color='teal'
+        style={{ border: 'none' }}
+      >
+        <Header>Chat about this event</Header>
+      </Segment>
+      <Segment attached clearing>
+        <Comment.Group>
+          {commentStore.comments.map((comment) => (
+            <Comment key={comment.id}>
+              <Comment.Avatar src={comment.image || '/assets/user.png'} />
+              <Comment.Content>
+                <Comment.Author as={Link} to={`/profiles/${comment.username}}`}>
+                  {comment.displayName}
+                </Comment.Author>
+                <Comment.Metadata>
+                  <div>{comment.createdAt}</div>
+                </Comment.Metadata>
+                <Comment.Text>{comment.body}</Comment.Text>
+              </Comment.Content>
+            </Comment>
+          ))}
+
+          <Formik
+            onSubmit={(values, { resetForm }) =>
+              commentStore.addComments(values).then(() => resetForm())
+            }
+            initialValues={{ body: '' }}
+          >
+            {({ isSubmitting, isValid }) => (
+              <Form className='ui form'>
+                <MyTextArea placeholder='Add Comment' name='body' rows={2} />
+                <Button
+                  loading={isSubmitting}
+                  disabled={isSubmitting || !isValid}
+                  content='Add Reply'
+                  labelPosition='left'
+                  icon='edit'
+                  primary
+                  type='submit'
+                  floated='right'
+                />
+              </Form>
+            )}
+          </Formik>
+        </Comment.Group>
+      </Segment>
+    </>
+  );
+});
